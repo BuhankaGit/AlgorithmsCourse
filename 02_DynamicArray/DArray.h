c3edeb13f33033a6e539a3bfacfc9abe704e587f
Cyganov Gleb <67660146+BuhankaGit@users.noreply.github.com>
2022-12-19 20:47:18 +0300

    Moved the implementation to the h file

--- 02_DynamicArray/DArray.h
+++ 02_DynamicArray/DArray.h
@@ -1,4 +1,3 @@
-#pragma once
 #include <iostream>
 #include <stdlib.h>
 #ifndef DARRAY_H
@@ -10,6 +9,7 @@ class DArray final
 private:
 	T* array;
 	int capacity;
+	int DEFAULT_CAPACITY = 8;
 	int length;
 
 public:
@@ -18,11 +18,13 @@ public:
 		int current;
 		DArray* arr;
 		bool isForward;
+		int direction;
 	public:
 		Iterator(DArray* array, bool isForward) {
 			this->isForward = isForward;
 			current = isForward ? 0 : array -> size()-1;
 			arr = array;
+			direction = isForward ? 1 : -1;
 		}
 
 		const T& get() const {
@@ -30,7 +32,6 @@ public:
 		};
 
 		void next() {
-			int direction = isForward ? 1 : -1;
 			current += direction;
 		};
 
@@ -42,6 +43,12 @@ public:
 			if (isForward) return (current + 1) < arr -> size();
 			else return (current - 1) >= 0;
 		}
+
+		Iterator& operator++() { current += direction; return *this; };
+		Iterator operator++(int d) { Iterator buff = *this; current += direction; return buff; };
+
+		Iterator& operator--() { current -= direction; return *this; };
+		Iterator operator--(int d) { Iterator buff = *this; current -= direction; return buff; };
 	};
 
 	DArray();
@@ -97,14 +104,124 @@ public:
 		array = other.array;
 
 		other.array = nullptr;
-		other.capacity = 8;
+		other.capacity = DEFAULT_CAPACITY;
 		other.length = 0;
 
 		return *this;
 	};
 };
 
-#include "DArray.cpp"
+template<typename T>
+DArray<T>::DArray()
+{
+	this->capacity = 8;
+	this->length = 0;
+	array = (T*)malloc(sizeof(T) * capacity);
+}
+
+template<typename T>
+DArray<T>::DArray(int capacity)
+{
+	this->capacity = capacity;
+	this->length = 0;
+	array = (T*)malloc(sizeof(T) * capacity);
+}
+
+template<typename T>
+DArray<T>::DArray(const DArray& other)
+{
+	capacity = other.capacity;
+	length = other.length;
+	array = (T*)malloc(sizeof(T) * capacity);
+	for (int i = 0; i < length; i++) {
+		new(array + i) T(other.array[i]);
+	}
+}
+
+template<typename T>
+DArray<T>::DArray(DArray&& other) noexcept
+{
+	capacity = other.capacity;
+	length = other.length;
+	free(array);
+	array = other.array;
+	other.length = 0;
+	other.capacity = 8;
+	other.array = nullptr;
+}
+
+template<typename T>
+void DArray<T>::resize(int newSize)
+{
+	T* newArray = (T*)malloc(sizeof(T) * newSize);
+
+	for (int i = 0; i < capacity; i++) {
+		new(newArray + i) T(std::move(array[i]));
+		array[i].~T();
+	}
+	free(array);
+
+	capacity = newSize;
+	array = newArray;
+	newArray = nullptr;
+}
+
+template<typename T>
+int DArray<T>::insert(const T& value)
+{
+	if (length == capacity) {
+		this->resize(capacity * 2);
+	}
+	new(array + length) T(value);
+	length += 1;
+	return length - 1;
+}
+
+template<typename T>
+int DArray<T>::insert(int index, const T& value)
+{
+	if (index < 0 || index >= length) {
+		return -1;
+	}
+	if (length == capacity) {
+		this->resize(capacity * 2);
+	}
+	for (int i = length; i >= index; i--) {
+		new(array + i + 1) T(std::move(array[i]));
+	}
+	new(array + index) T(value);
+	length += 1;
+	return index;
+}
+
+template<typename T>
+void DArray<T>::remove(int index)
+{
+	if (index < 0 || index > length) {
+		return;
+	}
+	for (int i = index; i < length - 1; i++) {
+		array[i].~T();
+		new(array + i) T(std::move(array[i + 1]));
+	}
+	array[length - 1].~T();
+	length -= 1;
+}
+
+template<typename T>
+int DArray<T>::size()
+{
+	return length;
+}
+
+template<typename T>
+DArray<T>::~DArray()
+{
+	for (int i = 0; i < length; i++) {
+		array[i].~T();
+	}
+	free(array);
+}
 
 
 #endif
