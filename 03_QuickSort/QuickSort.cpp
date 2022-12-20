1f257dcf70fa09dda9a8385b7cae2d31a96a9ede
Cyganov Gleb <67660146+BuhankaGit@users.noreply.github.com>
2022-12-20 10:20:57 +0300

    Added readme

--- 03_QuickSort/QuickSort.cpp
+++ 03_QuickSort/QuickSort.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <random>
+#include <chrono>
+#include <string>
 
 template<typename T, typename Compare>
 void sort(T* first, T* last, Compare comp)
 {
-    int optimSize = 10;
+    int optimSize = 42;
     while (first < last)
     {
         if (last - first < optimSize)
@@ -14,10 +16,10 @@ void sort(T* first, T* last, Compare comp)
         }
         else {
             T* medium = first + (last - first) / 2;
-            if(comp(*last, *first)) std::swap(*last, *first);
-            if(comp(*medium, *first)) std::swap(*medium, *first);
-            if(comp(*last, *medium)) std::swap(*last, *medium);
-            
+            if (comp(*last, *first)) std::swap(*last, *first);
+            if (comp(*medium, *first)) std::swap(*medium, *first);
+            if (comp(*last, *medium)) std::swap(*last, *medium);
+
             T* q = partition(first, last, comp);
 
             if (q - first < last - q)
@@ -25,7 +27,7 @@ void sort(T* first, T* last, Compare comp)
                 sort(first, q, comp);
                 first = q + 1;
             }
-            else 
+            else
             {
                 sort(q + 1, last, comp);
                 last = q;
@@ -76,6 +78,33 @@ void insertion(T* first, T* last, Compare comp)
 }
 
 int main()
-{
-    exit(0);
+{\
+    
+    /**
+    std::default_random_engine gen;
+    std::uniform_int_distribution<int> distr(-1000, 1000);
+
+    for (int size = 2; size <= 100; size++)
+    {
+        std::vector<int> test_qs(size);
+        std::vector<int> test_is(size);
+        for (int i = 0; i < size; ++i)
+        {
+            test_qs[i] = distr(gen);
+            test_is[i] = test_qs[i];
+        }
+        auto begin = std::chrono::high_resolution_clock::now();
+        sort(&test_qs[0], &test_qs[size - 1], [](int a, int b) { return a < b; });
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> diff = end - begin;
+        std::cout << std::to_string(diff.count() * 10000.0) << " ";
+        auto begin2 = std::chrono::high_resolution_clock::now();
+        insertion(&test_is[0], &test_is[size - 1], [](int a, int b) { return a < b; });
+        auto end2 = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> diff2 = end2 - begin2;
+        std::cout << std::to_string(diff2.count() * 10000.0) << std::endl;
+    }
+    **/
+
+    return 0;
 }
---
+++ 03_QuickSort/Readme.md
@@ -0,0 +1,9 @@
+## QuickSort
+Implementation of the QuickSort algorithm
+
+The algorithm uses the following optimization: for intervals shorter than 42, an insertion sort algorithm is used.
+
+The optimal length of the intervals is obtained empirically. The data and graph are presented below
+
+Link: [data](https://docs.google.com/spreadsheets/d/1irLY2U4ibOBCi2QEWkRfwkftVZh7XyhtZ-5XnLl0ir8/edit?usp=sharing)
+
